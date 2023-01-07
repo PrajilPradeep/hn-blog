4e925d1a27bec5bacc50c1f38dc59db49ee09973
Prajil <Prajil7425@gmail.com>
2023-01-07 17:41:56 +0530

    Copy code for custom hook from Home component to useFetch component

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,5 +1,28 @@
 const useFetch = () => {
+    
+    const [blogs,setBlogs] = useState(null);
+    const [isPending,setIsPending] = useState(true);
+    const [error,setError] = useState(null);
 
+    useEffect(()=>{
+        fetch("http://localhost:8000/blogs")
+        .then((res) => {
+            if(!res.ok){
+                throw Error("Failed to fetch data for the resource");
+            }
+            // console.log(res);
+            return res.json();
+        })
+        .then ((data)=>{
+            // console.log(data);
+            setBlogs(data);
+            setIsPending(false); 
+            setError(null); /* To remove previous error message (If exist) for subsequent requests.*/
+        })
+        .catch((err)=> {
+            setIsPending(false); /*To remove loading message on getting an error*/
+            setError(err.message)}); 
+    },[]);
 
 }
 
