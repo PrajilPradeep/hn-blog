f8fb963e4d3e1e9125fa93705b84491d88bd6be0
Prajil <Prajil7425@gmail.com>
2023-01-07 17:48:23 +0530

    Set url variable instead of harcoding the url

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,13 +1,13 @@
 import {useState, useEffect} from 'react';
 
-const useFetch = () => {
+const useFetch = (url) => {
 
     const [data,setData] = useState(null);
     const [isPending,setIsPending] = useState(true);
     const [error,setError] = useState(null);
 
     useEffect(()=>{
-        fetch("http://localhost:8000/blogs")
+        fetch(url)
         .then((res) => {
             if(!res.ok){
                 throw Error("Failed to fetch data for the resource");
