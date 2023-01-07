78f0276c0333ef8a151b7ef9a5483635cd77315d
Prajil <Prajil7425@gmail.com>
2023-01-08 00:45:41 +0530

    Add path for create new blog

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './Navbar';
 import Home from './Home';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Create from './Create';
 
 function App() {
 
@@ -11,6 +12,9 @@ function App() {
         <div className="content">
           <Switch path="/">
             <Home />
+          </Switch>
+          <Switch path="/create">
+            <Create />
           </Switch>
         </div>
       </div>
