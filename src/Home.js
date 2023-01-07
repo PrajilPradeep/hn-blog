7fbb6a709e59972a137a1708cebd90d412acccc5
Prajil <Prajil7425@gmail.com>
2023-01-07 23:38:35 +0530

    Add dependency array value as url

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
 
+    const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
     return ( 
         <div className="home">
-            { error && <div className="errorMessage">{error}</div> }
+            { error && <div className="errorMessage"> {error}</div> }
             { isPending && <div className="loading">Loading...!</div> }
             { blogs && <BlogList blogs = {blogs} title="All blogs!" />}
         </div>   
--- src/useFetch.js
+++ src/useFetch.js
@@ -12,11 +12,11 @@ const useFetch = (url) => {
             if(!res.ok){
                 throw Error("Failed to fetch data for the resource");
             }
-            // console.log(res);
+            //console.log(res);
             return res.json();
         })
         .then ((data)=>{
-            // console.log(data);
+            //console.log(data);
             setData(data);
             setIsPending(false); 
             setError(null); /* To remove previous error message (If exist) for subsequent requests.*/
@@ -24,9 +24,9 @@ const useFetch = (url) => {
         .catch((err)=> {
             setIsPending(false); /*To remove loading message on getting an error*/
             setError(err.message)}); 
-    },[]);
+    },[url]);
 
-    return {data, isPending, error}; //to return state properties as js object
+    return {data,isPending, error}; //to return state properties as js object
 
 }
 
