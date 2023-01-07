4c0aae5c34fc8eb3f9001df998c5e44afa5ac508
Prajil <Prajil7425@gmail.com>
2023-01-07 13:05:26 +0530

    Create basic useCase hook

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -14,10 +14,16 @@ const Home = () => {
         const newBlogs =  blogs.filter(blog=> blog.id != id);
         setBlogs(newBlogs);
     }
+
+    useEffect(()=>{
+        console.log("useEffect hook is called !");
+    },[]);
+
     
+
     return ( 
         <div className="home">
-           <BlogList blogs = {blogs} title="All blogs!" handleDeleteBlog={handleDeleteBlog}/>  
+           <BlogList blogs = {blogs} title="All blogs!" handleDeleteBlog={handleDeleteBlog}/> 
         </div>   
      );
     }
