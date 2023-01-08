dcaf6310b0e935a448ba2652ea4500df341eefed
Prajil <Prajil7425@gmail.com>
2023-01-08 18:01:34 +0530

    Fetch data for required blog and display it

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,20 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
+    
     const { id } = useParams();
+    const {data: blog, isPending, error} = useFetch('http://localhost:8000/blogs/'+id);
+     
     return ( 
         <div className="blog-details">
-            <h2>Blog Details - { id }</h2>
+            { isPending && <p>Loading...!</p> }
+            { error && <p>{ error }</p>}
+            { blog && <article>
+                <h2>{ blog.title }</h2>
+                <p>Written by: { blog.author }</p>
+                <div>{ blog.body }</div>
+            </article> }
         </div>
      );
 }
